ecb685f7a0f0cdc13969de12e64c00a7667d934d
JOSE MANUEL MEDINA CANO <josemedina224120@correo.itm.edu.co>
2021-09-05 14:46:58 -0500

    TodoEntity changes

---
+++ todoConsolidate/todoConsolidate.Common/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoConsolidate.Common.Models
+{
+    public class Todo
+    {
+        public DateTime Timestamp { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
---
+++ todoConsolidate/todoConsolidate.Function/Entities/TodoEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoConsolidate.Function.Entities
+{
+    class TodoEntity : TableEntity
+    {
+        public DateTime Timestamp { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
